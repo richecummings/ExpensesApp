c3161cd4414d4b230468c89bb8eaeed5ee72f306
Richard Cummings <richardecummings@gmail.com>
2019-02-12 13:50:06 -0500

    Binding from code

--- ExpensesApp/Views/NewExpensePage.xaml.cs
+++ ExpensesApp/Views/NewExpensePage.xaml.cs
@@ -1,15 +1,44 @@
 ﻿using System;
 using System.Collections.Generic;
-
+using ExpensesApp.ViewModels;
 using Xamarin.Forms;
 
 namespace ExpensesApp.Views
 {
     public partial class NewExpensePage : ContentPage
     {
+        NewExpenseVM ViewModel;
+
         public NewExpensePage()
         {
             InitializeComponent();
+
+            ViewModel = Resources["vm"] as NewExpenseVM;
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+
+            ViewModel.GetExpenseStatus();
+
+            int count = 0;
+            var section = new TableSection("Statuses");
+            foreach (var es in ViewModel.ExpenseStatuses)
+            {
+                var cell = new SwitchCell { Text = es.Name };
+
+                Binding binding = new Binding();
+                binding.Source = ViewModel.ExpenseStatuses[count];
+                binding.Path = "Status";
+                binding.Mode = BindingMode.TwoWay;
+                cell.SetBinding(SwitchCell.OnProperty, binding);
+
+                section.Add(cell);
+
+                count++;
+            }
+            expenseTableView.Root.Add(section);
         }
     }
 }
